77871da0d5712ad7a7fc07c86deedd2e7dc12adf
NekitWebDevelop <79455513+NekitWebDevelop@users.noreply.github.com>
2021-07-09 02:25:47 +0300

    Add files via upload

---
+++ init.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+
+	$('.owl-carousel').owlCarousel({
+	    loop:false,
+	    margin:0,
+	    nav:true,
+	    items:1,
+	    autoHeight:true,
+	    dots:false,
+	    navSpeed:300
+	})
+    
+     $('.element-1').lightGallery({
+
+    });
+    
+  $("a.catalog1").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
+
+     $("a.about1").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+});
+         
+    $("a.stages1").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
+    
+    $("a.vorteile1").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
+    
+    $("a.gallery1").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
+    
+     $("a.contacts1").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
+});
